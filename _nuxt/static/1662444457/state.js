e6e8a8aa5e0a6f7380b29419ff1e0653973ddf69
saito-northdetail <saito-northdetail@users.noreply.github.com>
2022-09-06 07:56:14 +0000

    deploy: b499b5cc3fe131f6b1f78d34b4ea9e75e46059ab

--- _nuxt/static/1662444457/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fui-parts-collection\u002F_nuxt\u002Fstatic\u002F1662444457",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fui-parts-collection\u002F",assetsPath:"\u002Fui-parts-collection\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1662450972/hamburger/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fui-parts-collection\u002F_nuxt\u002Fstatic\u002F1662450972",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhamburger",config:{_app:{basePath:"\u002Fui-parts-collection\u002F",assetsPath:"\u002Fui-parts-collection\u002F_nuxt\u002F",cdnURL:a}}}}(null));
